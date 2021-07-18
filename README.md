d91d36fc373413f6b15049b51ea07ee11cadf755
Andre Roque <ajroque10@gmail.com>
2021-07-17 22:04:40 -0300

    Subindo o projeto para o GitHub

---
+++ README.md
@@ -0,0 +1,9 @@
+Landing Page - Black Friday
+
+Desafio #1 - Hiring Coders #2 - Gama Academy
+
+Projeto desenvolvido para o primeiro desafio da formação FullStack do Hiring Coders - plataforma Gama Academy.
+
+Objetivo: construir uma Landing Page de promoções estilo Black Friday, onde o usuário pode cadastrar seu nome e e-mail e os dados ficam salvos no Local Storage.
+
+Tecnologias utilizadas: HTML5, CSS3 e JavaScript.
---
+++ src/script.js
@@ -0,0 +1,18 @@
+onload = function() {
+
+    const form = document.getElementById('form')
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let nome = document.getElementById('nome').value;
+        let email = document.getElementById('email').value;
+        let data = {
+            nome,
+            email,
+        }
+        let convertData = JSON.stringify(data);
+
+        localStorage.setItem('lead', convertData);
+        location.reload()
+    })
+}
